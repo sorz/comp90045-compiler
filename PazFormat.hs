4098fcf71de9fcda7a56b45674c1e677ec96cba5
sorz <me@sorz.org>
2018-04-09 22:05:05 +1000

    Refactor import & fix all import error
    Remove LOTS of annoying imports

--- PazFormat.hs
+++ PazFormat.hs
@@ -2,44 +2,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 module PazFormat where
-import PazLexer (
-    ASTIdentifier,
-    ASTUnsignedInteger,
-    )
-import PazParser (
-    ASTProgram,
-    ASTVariableDeclarationPart,
-    ASTProcedureDeclarationPart,
-    ASTProcedureDeclaration,
-    ASTCompoundStatement,
-    ASTVariableDeclaration,
-    ASTIdentifierList,
-    ASTTypeDenoter,
-    ASTTypeIdentifier,
-    ASTArrayType,
-    ASTConstant,
-    ASTSubrangeType,
-    ASTSign,
-    ASTFormalParameterList,
-    Sign(..),
-    TypeDenoter(..),
-    TypeIdentifier(..),
-    )
-
-import PazParser (
-    ASTExpression,
-    ASTRelationalOperator,
-    ASTSimpleExpression,
-    ASTAddingOperator,
-    ASTTerm,
-    ASTMutiplayingOperator,
-    ASTFactor,
-    ASTVariableAccess,
-    ASTIndexedVariable,
-    ASTUnsignedNumber,
-    ASTUnsignedConstant,
-    )
-
+import PazLexer as L
+import PazParser as P
 
 class PrettyPrint ast where
     prettyPrint :: ast -> String
@@ -48,35 +12,35 @@ class PrettyPrint ast where
 -- ASTProgram =
 --   (ASTIdentifier, ASTVariableDeclarationPart,
 --    ASTProcedureDeclarationPart, ASTCompoundStatement)
-instance PrettyPrint ASTProgram where
+instance PrettyPrint P.ASTProgram where
     prettyPrint (id, variable, procedure, statement) =
         "program " ++ (prettyPrint id) ++ ";\n\n" ++
         (prettyPrint variable) ++
         (prettyPrint procedure) ++
         (prettyPrint statement)
 
-instance PrettyPrint ASTCompoundStatement where 
+instance PrettyPrint P.ASTCompoundStatement where 
     prettyPrint = show  
 
 -- ASTVariableDeclarationPart = [ASTVariableDeclaration]
-instance PrettyPrint ASTVariableDeclarationPart where
+instance PrettyPrint P.ASTVariableDeclarationPart where
     prettyPrint [] = ""
     prettyPrint xs =
         "var\n" ++ (showVariableDeclarationPart xs)
 
-showVariableDeclarationPart :: ASTVariableDeclarationPart -> String
+showVariableDeclarationPart :: P.ASTVariableDeclarationPart -> String
 showVariableDeclarationPart [] = ""
 showVariableDeclarationPart (x:xs) =
     "    " ++ (prettyPrint x) ++ "\n"
     ++ showVariableDeclarationPart xs
 
 -- ASTVariableDeclaration = (ASTIdentifierList, ASTTypeDenoter)
-instance PrettyPrint ASTVariableDeclaration where
+instance PrettyPrint P.ASTVariableDeclaration where
     prettyPrint (ids, typ) =
         (prettyPrint ids) ++ ": " ++ (prettyPrint typ) ++ ";"
 
 -- ASTIdentifierList = [ASTIdentifier]
-instance PrettyPrint ASTIdentifierList where
+instance PrettyPrint P.ASTIdentifierList where
     prettyPrint [] = ""
     prettyPrint (x:[]) = id x
     prettyPrint (x:xs) =
@@ -84,68 +48,65 @@ instance PrettyPrint ASTIdentifierList where
 
 -- ASTIdentifier = String
 -- Use `id` instead of `show` to avoid quotes ("").
-instance PrettyPrint ASTIdentifier where
+instance PrettyPrint L.ASTIdentifier where
     prettyPrint = id
 
 -- ASTTypeDenoter = OrdinaryTypeDenoter | ArrayTypeDenoter
-instance PrettyPrint ASTTypeDenoter where
+instance PrettyPrint P.ASTTypeDenoter where
     prettyPrint (OrdinaryTypeDenoter x) = prettyPrint x
     prettyPrint (ArrayTypeDenoter x) = prettyPrint x
 
-instance PrettyPrint ASTTypeIdentifier where
+instance PrettyPrint P.ASTTypeIdentifier where
     prettyPrint IntegerTypeIdentifier = "integer"
     prettyPrint RealTypeIdentifier = "real"
     prettyPrint BooleanTypeIdentifier = "boolean"
 
-instance PrettyPrint ASTArrayType where
+instance PrettyPrint P.ASTArrayType where
     prettyPrint (subTyp, typId) =
         "array [" ++ (prettyPrint subTyp) ++ "] of " ++
         (prettyPrint typId)
 
-instance PrettyPrint ASTSubrangeType where
+instance PrettyPrint P.ASTSubrangeType where
     prettyPrint (const1, const2) =
         (prettyPrint const1) ++ ".." ++ (prettyPrint const2)
 
-instance PrettyPrint ASTConstant where
+instance PrettyPrint P.ASTConstant where
     prettyPrint (Nothing, n) = prettyPrint n
     prettyPrint ((Just sign), n) =
         (prettyPrint sign) ++ (prettyPrint n)
 
-instance PrettyPrint ASTUnsignedInteger where
+instance PrettyPrint L.ASTUnsignedInteger where
     prettyPrint = show
 
-instance PrettyPrint ASTSign where
-    prettyPrint SignPlus = "+"
-    prettyPrint SignMinus = "-"
+instance PrettyPrint P.ASTSign where
+    prettyPrint P.SignPlus = "+"
+    prettyPrint P.SignMinus = "-"
 
-instance PrettyPrint ASTProcedureDeclarationPart where
+instance PrettyPrint P.ASTProcedureDeclarationPart where
     prettyPrint [] = ""
     prettyPrint (x:xs) =
         (prettyPrint x) ++ "\n" ++ (prettyPrint xs)
 
-instance  PrettyPrint ASTProcedureDeclaration where
-    prettyPrint = show 
-
 -- ASTFormalParameterList = (ASTFormalParameterSection, [ASTFormalParameterSection])
-instance PrettyPrint ASTFormalParameterList where
+instance PrettyPrint P.ASTFormalParameterList where
     prettyPrint (section, sections) = "(" ++ (show section)++ (show sections)++ "\n)"
 
 -- ASTProcedureDeclaration = (ASTIdentifier, (Maybe ASTFormalParameterList), ASTVariableDeclarationPart, ASTCompoundStatement)
-instance PrettyPrint ASTProcedureDeclaration where
+instance PrettyPrint P.ASTProcedureDeclaration where
     prettyPrint (id, param, var, stat) = 
         case param of 
             Nothing -> "procedure " ++ (prettyPrint id) ++ "(" ++ " nothing " ++ ")\n" ++ (show var) ++ (show stat) 
             Just param -> "procedure " ++ (prettyPrint id) ++ "(" ++ (show x) ++ ")\n" ++ (show var) ++ (show stat) 
 
 -- ASTExpression = (ASTSimpleExpression, Maybe (ASTRelationalOperator, ASTSimpleExpression))
-instance PrettyPrint ASTExpression where
+instance PrettyPrint P.ASTExpression where
     prettyPrint (astSimpExp1, param)= 
         case param of 
             Nothing -> astSimpExp1
             Just param ->  astSimpExp1 ++ param
 
 -- ASTRelationalOperator = RelationalOperator
-instance PrettyPrint ASTRelationalOperator where
+instance PrettyPrint P.ASTRelationalOperator where
     prettyPrint reloperator =
         case reloperator of 
             Equal -> "EQUAL" ++ "\n;"
@@ -156,66 +117,66 @@ instance PrettyPrint ASTRelationalOperator where
             GreaterThanOrEqual -> "GREATER_THAN_OR_EQUAL" ++ "\n;"
 
 -- -- ASTSimpleExpression = (Maybe Sign, ASTTerm, [(ASTAddingOperator, ASTTerm)])
-instance PrettyPrint ASTSimpleExpression where
+instance PrettyPrint P.ASTSimpleExpression where
     prettyPrint (sign, astTerm1, [(opeartor, astTerm2)]) =
         case sign of 
             Nothing -> astTerm1 ++ "{" ++ opeartor ++ "}" ++ "\n;"
             Just sign -> "[" ++ sign ++ "]" ++ astTerm1 ++ "{" ++ opeartor ++ "}" ++ "\n;"
 
 -- -- ASTAddingOperator = AddingOperator
-instance PrettyPrint ASTAddingOperator where
+instance PrettyPrint P.ASTAddingOperator where
     prettyPrint opeartor= 
         case operator of 
             Plus -> "+" ++ "\n;"
             Minus -> "-" ++ "\n;"
 
 -- -- ASTTerm = (ASTFactor, [(ASTMutiplayingOperator, ASTFactor)])
-instance PrettyPrint ASTTerm where
+instance PrettyPrint P.ASTTerm where
     prettyPrint (factor1, [(opeartor, factor2)]) =  
         factor1 ++ " {" ++ opeartor ++ " " ++ "factor2" ++ " }" ++ "\n;"
 
 -- type ASTMutiplayingOperator = MutiplayingOperator
-instance PrettyPrint ASTMutiplayingOperator where
+instance PrettyPrint P.ASTMutiplayingOperator where
     prettyPrint multiOperator= 
         case  multiOperator of 
-            Times -> "TIMES" ++ "\n;"
-            Divideby -> "DIVIDE_BY" ++ "\n;"
-            Div -> "DIV" ++ "\n;"
-            And -> "AND" ++ "\n;"
+            P.Times -> "TIMES" ++ "\n;"
+            P.DivideBy -> "DIVIDE_BY" ++ "\n;"
+            P.Div -> "DIV" ++ "\n;"
+            P.And -> "AND" ++ "\n;"
 
 -- ASTFactor = Factor
-instance PrettyPrint ASTFactor where
+instance PrettyPrint P.ASTFactor where
     prettyPrint factor = 
         case factor of
-            UnsignedConstant ASTUnsignedConstant -> "unsigned_constant" ++ "\n;"
-            VariableAccess ASTVariableAccess -> "variable_access" ++ "\n;"
-            Expression ASTExpression -> "LEFT_PARENTHESIS expression RIGHT_PARENTHESIS" ++ "\n;"
-            NotFactor ASTFactor -> "NOT factor" ++ "\n;"
+            UnsignedConstant _ -> "unsigned_constant" ++ "\n;"
+            VariableAccess _ -> "variable_access" ++ "\n;"
+            Expression _ -> "LEFT_PARENTHESIS expression RIGHT_PARENTHESIS" ++ "\n;"
+            NotFactor _ -> "NOT factor" ++ "\n;"
 
 
 -- -- ASTVariableAccess = VariableAccess
-instance PrettyPrint ASTVariableAccess where
+instance PrettyPrint P.ASTVariableAccess where
     prettyPrint sign =
         case sing of 
-            IndexedVariable ASTIndexedVariable -> "indexed_variable" ++ "\n;"
-            Identifier ASTIdentifier ->  "identifier" ++ "\n;"
+            IndexedVariable _ -> "indexed_variable" ++ "\n;"
+            Identifier _ ->  "identifier" ++ "\n;"
 
 -- -- ASTIndexedVariable = (ASTIdentifier, ASTExpression)
-instance PrettyPrint ASTIndexedVariable where
+instance PrettyPrint P.ASTIndexedVariable where
     prettyPrint (id, astExp) = 
         id ++ " LEFT_BRACKET " ++ astExp ++ " RIGHT_BRACKET " ++ "\n;"
 
 -- -- ASTUnsignedNumber = UnsignedNumber
-instance PrettyPrint ASTUnsignedNumber where
+instance PrettyPrint P.ASTUnsignedNumber where
     prettyPrint unsignedNum = 
         case  unsignedNum of 
-            UnsignedInteger ASTUnsignedInteger -> "unsigned_integer" ++ "\n;"
-            UnsignedReal ASTUnsignedReal -> "unsigned_real" ++ "\n;"
+            UnsignedInteger _ -> "unsigned_integer" ++ "\n;"
+            UnsignedReal _ -> "unsigned_real" ++ "\n;"
 
 
 -- -- ASTUnsignedConstant = UnsignedConstant
-instance PrettyPrint ASTUnsignedConstant where
+instance PrettyPrint P.ASTUnsignedConstant where
     prettyPrint unsignedCon = 
         case unsignedCon of 
-            UnsignedNumber ASTUnsignedNumber -> " unsigned_number" ++ "\n;"
-            CharacterString ASTCharacterString -> " unsigned_real" ++ "\n;"
+            UnsignedNumber _ -> " unsigned_number" ++ "\n;"
+            CharacterString _ -> " unsigned_real" ++ "\n;"
